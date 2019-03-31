2a6e724eff503ce44fdebc001b02d55b41e5ec08
zxc <512724412@qq.com>
2019-03-31 14:09:11 +0800

    Initial commit

---
+++ src/com/zxc/array/Solution_01.java
@@ -0,0 +1,78 @@
+package array;
+
+/**
+ * @author zxc
+ * @date 2019-03-30 20:56
+ * 题目描述：
+ * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ */
+public class Solution_01 {
+    public static void main(String[] args) {
+        int array[][] = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9,}
+        };
+
+        System.out.println(find2(array, 3));
+    }
+
+    /**
+     *  解题思想：
+     * 目标值大于右上角的值，下移
+     * 目标值小于右上角的值，左移
+     * 如果越界，说明不在数组中
+     * @param array
+     * @param target
+     * @return
+     */
+    public static boolean find(int array[][], int target) {
+        if (array == null || array.length == 0 || array[0].length == 0) return false;
+
+        int row = 0;  //行
+        int col = array[0].length - 1; //列
+        //因为row是增加的，所以只要小于数组行的长度就可以
+        //因为col是减少的，所以只要>=0就可以了
+        while (row < array.length && col >= 0) {
+            int temp = array[row][col];  //右上角的值，初始为[0,2]
+            if (target == temp) {
+                return true;
+            } else if (target < temp) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 把每一行看成有序递增的数组，
+       利用二分查找，
+       通过遍历每一行得到答案，
+       时间复杂度是nlogn
+     * @param array
+     * @param target
+     * @return
+     */
+    public static boolean find2(int array[][], int target) {
+        if (array == null || array.length == 0 || array[0].length == 0) return false;
+
+        for (int i = 0; i < array.length; i++) {
+            int low = 0;
+            int high = array[i].length-1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (target == array[i][mid]) {
+                    return true;
+                } else if (target < array[i][mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/com/zxc/array/Solution_02.java
@@ -0,0 +1,57 @@
+package array;
+
+/**
+ * @author zxc
+ * @date 2019-03-30 22:49
+ * 旋转数组的最小数字
+ * 题目描述:
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+ * 输入一个非减排序的数组的一个旋转，输出旋转数组的最小元素。
+ * 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。
+ * NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+ */
+public class Solution_02 {
+    public static void main(String[] args) {
+        int array[] = {0,1,1,1,1};
+        System.out.println(find((array)));
+    }
+
+    public static int find(int[] array) {
+        if (array.length == 0 || array == null) return 0;
+        int right = array.length - 1;
+        int left = 0;
+        //旋转条件
+        while (array[left] >= array[right]) {
+            //循环终止条件
+            if (right - 1 == left) {
+                return array[right];
+            }
+
+            int mid = left + ((right - left) >> 1);
+            //中间的值大于左第一个值，最小的元素肯定位于中间元素后面，左指针指向中间
+            if (array[mid] > array[left]) {
+                left = mid;
+            }
+            //中间的值小于左第一个值，最小的元素肯定位于中间元素的前面，右指针指向中间
+            if (array[mid] < array[left]) {
+                right = mid;
+            }
+            //如果第一个元素等于最后元素和中间元素，不能判断中间元素位于前面递增数组还是后面递增数组，需要遍历查询
+            if (array[left] == array[mid] && array[left] == array[right]) {
+              return   findMin(left,right,array);
+            }
+
+        }
+        return 0;
+    }
+    public static int findMin(int left, int right, int array[]){
+        int min = array[left];
+        for (int i = left; i < right; i++) {
+            if(min > array[i+1]){
+                min=array[i+1];
+            }
+        }
+        return min;
+    }
+}
+
